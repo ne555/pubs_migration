0d38eaf796df27f746adf3e9b26a3ec30330dbc8
aldo_bedrij <wbedrij@gmail.com>
2014-10-06 09:02:34 -0300

    use format YY/MM/DD

--- pubs_sqlite.sql
+++ pubs_sqlite.sql
@@ -65,10 +65,9 @@ create TABLE titles
    notes          varchar(200)          NULL,
 
    pubdate        date          NOT NULL
-	default ('01/01/1900')
+	default ('1900/01/01')
 );
--- hate yankeeland and its stupid convention
-set datestyle = MDY;
+set datestyle = YMD;
 
 CREATE TABLE titleauthor
 (
@@ -310,72 +309,72 @@ This is sample text data for Lucerne Publishing, publisher 9999 in the pubs data
 insert into titles values ('PC8888', 'Secrets of Silicon Valley', 'popular_comp', '1389',
 20.00, 8000.00, 10, 4095,
 'Muckraking reporting on the world''s largest computer hardware and software manufacturers.',
-'06/12/94');
+'94/06/12');
 
 insert into titles values ('BU1032', 'The Busy Executive''s Database Guide', 'business',
 '1389', 19.99, 5000.00, 10, 4095,
 'An overview of available database systems with emphasis on common business applications. Illustrated.',
-'06/12/91');
+'91/06/12');
 
 insert into titles values ('PS7777', 'Emotional Security: A New Algorithm', 'psychology',
 '0736', 7.99, 4000.00, 10, 3336,
 'Protecting yourself and your loved ones from undue emotional stress in the modern world. Use of computer and nutritional aids emphasized.',
-'06/12/91');
+'91/06/12');
 
 insert into titles values ('PS3333', 'Prolonged Data Deprivation: Four Case Studies',
 'psychology', '0736', 19.99, 2000.00, 10, 4072,
 'What happens when the data runs dry?  Searching evaluations of information-shortage effects.',
-'06/12/91');
+'91/06/12');
 
 insert into titles values ('BU1111', 'Cooking with Computers: Surreptitious Balance Sheets',
 'business', '1389', 11.95, 5000.00, 10, 3876,
 'Helpful hints on how to use your electronic resources to the best advantage.',
-'06/09/91');
+'91/06/09');
 
 insert into titles values ('MC2222', 'Silicon Valley Gastronomic Treats', 'mod_cook', '0877',
 19.99, 0.00, 12, 2032,
 'Favorite recipes for quick, easy, and elegant meals.',
-'06/09/91');
+'91/06/09');
 
 insert into titles values ('TC7777', 'Sushi, Anyone?', 'trad_cook', '0877', 14.99, 8000.00,
 10, 4095,
 'Detailed instructions on how to make authentic Japanese sushi in your spare time.',
-'06/12/91');
+'91/06/12');
 
 insert into titles values ('TC4203', 'Fifty Years in Buckingham Palace Kitchens', 'trad_cook',
 '0877', 11.95, 4000.00, 14, 15096,
 'More anecdotes from the Queen''s favorite cook describing life among English royalty. Recipes, techniques, tender vignettes.',
-'06/12/91');
+'91/06/12');
 
 insert into titles values ('PC1035', 'But Is It User Friendly?', 'popular_comp', '1389',
 22.95, 7000.00, 16, 8780,
 'A survey of software for the naive user, focusing on the ''friendliness'' of each.',
-'06/30/91');
+'91/06/30');
 
 insert into titles values('BU2075', 'You Can Combat Computer Stress!', 'business', '0736',
 2.99, 10125.00, 24, 18722,
 'The latest medical and psychological techniques for living with the electronic office. Easy-to-understand explanations.',
-'06/30/91');
+'91/06/30');
 
 insert into titles values('PS2091', 'Is Anger the Enemy?', 'psychology', '0736', 10.95,
 2275.00, 12, 2045,
 'Carefully researched study of the effects of strong emotions on the body. Metabolic charts included.',
-'06/15/91');
+'91/06/15');
 
 insert into titles values('PS2106', 'Life Without Fear', 'psychology', '0736', 7.00, 6000.00,
 10, 111,
 'New exercise, meditation, and nutritional techniques that can reduce the shock of daily interactions. Popular audience. Sample menus included, exercise video available separately.',
-'10/05/91');
+'91/10/05');
 
 insert into titles values('MC3021', 'The Gourmet Microwave', 'mod_cook', '0877', 2.99,
 15000.00, 24, 22246,
 'Traditional French gourmet recipes adapted for modern microwave cooking.',
-'06/18/91');
+'91/06/18');
 
 insert into titles values('TC3218', 'Onions, Leeks, and Garlic: Cooking Secrets of the Mediterranean',
 'trad_cook', '0877', 20.95, 7000.00, 10, 375,
 'Profusely illustrated in color, this makes a wonderful gift book for a cuisine-oriented friend.',
-'10/21/91');
+'91/10/21');
 
 insert into titles (title_id, title, pub_id) values('MC3026',
 'The Psychology of Computer Cooking', '0877');
@@ -383,12 +382,12 @@ insert into titles (title_id, title, pub_id) values('MC3026',
 insert into titles values ('BU7832', 'Straight Talk About Computers', 'business', '1389',
 19.99, 5000.00, 10, 4095,
 'Annotated analysis of what computers can do for you: a no-hype guide for the critical user.',
-'06/22/91');
+'91/06/22');
 
 insert into titles values('PS1372', 'Computer Phobic AND Non-Phobic Individuals: Behavior Variations',
 'psychology', '0877', 21.59, 7000.00, 10, 375,
 'A must for the specialist, this book examines the difference between those who hate and fear computers and those who don''t.',
-'10/21/91');
+'91/10/21');
 
 insert into titles (title_id, title, type, pub_id, notes) values('PC9999', 'Net Etiquette',
 'popular_comp', '1389', 'A must-read for computer conferencing.');
@@ -428,33 +427,6 @@ insert into stores values('6380','Eric the Read Books','788 Catamaugus Ave.',
       'Seattle','WA','98056');
 insert into stores values('7896','Fricative Bookshop','89 Madison St.','Fremont','CA','90019');
 
--- MM/DD/YY
--- damn yankeeland
-insert into sales values('7066', 'QA7442.3', '09/13/94', 75, 'ON invoice','PS2091');
-insert into sales values('7067', 'D4482', '09/14/94', 10, 'Net 60','PS2091');
-insert into sales values('7131', 'N914008', '09/14/94', 20, 'Net 30','PS2091');
-insert into sales values('7131', 'N914014', '09/14/94', 25, 'Net 30','MC3021');
-insert into sales values('8042', '423LL922', '09/14/94', 15, 'ON invoice','MC3021');
-insert into sales values('8042', '423LL930', '09/14/94', 10, 'ON invoice','BU1032');
-insert into sales values('6380', '722a', '09/13/94', 3, 'Net 60','PS2091');
-insert into sales values('6380', '6871', '09/14/94', 5, 'Net 60','BU1032');
-insert into sales values('8042','P723', '03/11/93', 25, 'Net 30', 'BU1111');
-insert into sales values('7896','X999', '02/21/93', 35, 'ON invoice', 'BU2075');
-insert into sales values('7896','QQ2299', '10/28/93', 15, 'Net 60', 'BU7832');
-insert into sales values('7896','TQ456', '12/12/93', 10, 'Net 60', 'MC2222');
-insert into sales values('8042','QA879.1', '5/22/93', 30, 'Net 30', 'PC1035');
-insert into sales values('7066','A2976', '5/24/93', 50, 'Net 30', 'PC8888');
-insert into sales values('7131','P3087a', '5/29/93', 20, 'Net 60', 'PS1372');
-insert into sales values('7131','P3087a', '5/29/93', 25, 'Net 60', 'PS2106');
-insert into sales values('7131','P3087a', '5/29/93', 15, 'Net 60', 'PS3333');
-insert into sales values('7131','P3087a', '5/29/93', 25, 'Net 60', 'PS7777');
-insert into sales values('7067','P2121', '6/15/92', 40, 'Net 30', 'TC3218');
-insert into sales values('7067','P2121', '6/15/92', 20, 'Net 30', 'TC4203');
-insert into sales values('7067','P2121', '6/15/92', 20, 'Net 30', 'TC7777');
-
-
-/*
--- YY/MM/DD
 insert into sales values('7066', 'QA7442.3', '94/09/13', 75, 'ON invoice','PS2091');
 insert into sales values('7067', 'D4482', '94/09/14', 10, 'Net 60','PS2091');
 insert into sales values('7131', 'N914008', '94/09/14', 20, 'Net 30','PS2091');
@@ -467,17 +439,15 @@ insert into sales values('8042','P723', '93/03/11', 25, 'Net 30', 'BU1111');
 insert into sales values('7896','X999', '93/02/21', 35, 'ON invoice', 'BU2075');
 insert into sales values('7896','QQ2299', '93/10/28', 15, 'Net 60', 'BU7832');
 insert into sales values('7896','TQ456', '93/12/12', 10, 'Net 60', 'MC2222');
-insert into sales values('8042','QA879.1', '93/05/22', 30, 'Net 30', 'PC1035');
-insert into sales values('7066','A2976', '93/05/24', 50, 'Net 30', 'PC8888');
-insert into sales values('7131','P3087a', '93/05/29', 20, 'Net 60', 'PS1372');
-insert into sales values('7131','P3087a', '93/05/29', 25, 'Net 60', 'PS2106');
-insert into sales values('7131','P3087a', '93/05/29', 15, 'Net 60', 'PS3333');
-insert into sales values('7131','P3087a', '93/05/29', 25, 'Net 60', 'PS7777');
-insert into sales values('7067','P2121', '92/06/15', 40, 'Net 30', 'TC3218');
-insert into sales values('7067','P2121', '92/06/15', 20, 'Net 30', 'TC4203');
-insert into sales values('7067','P2121', '92/06/15', 20, 'Net 30', 'TC7777');
-*/
-
+insert into sales values('8042','QA879.1', '93/5/22', 30, 'Net 30', 'PC1035');
+insert into sales values('7066','A2976', '93/5/24', 50, 'Net 30', 'PC8888');
+insert into sales values('7131','P3087a', '93/5/29', 20, 'Net 60', 'PS1372');
+insert into sales values('7131','P3087a', '93/5/29', 25, 'Net 60', 'PS2106');
+insert into sales values('7131','P3087a', '93/5/29', 15, 'Net 60', 'PS3333');
+insert into sales values('7131','P3087a', '93/5/29', 25, 'Net 60', 'PS7777');
+insert into sales values('7067','P2121', '92/6/15', 40, 'Net 30', 'TC3218');
+insert into sales values('7067','P2121', '92/6/15', 20, 'Net 30', 'TC4203');
+insert into sales values('7067','P2121', '92/6/15', 20, 'Net 30', 'TC7777');
 
 insert into roysched values('BU1032', 0, 5000, 10);
 insert into roysched values('BU1032', 5001, 50000, 12);
@@ -593,50 +563,50 @@ insert into jobs (job_desc, min_lvl, max_lvl) values ('Editor', 25, 100);
 insert into jobs (job_desc, min_lvl, max_lvl) values ('Sales Representative', 25, 100);
 insert into jobs (job_desc, min_lvl, max_lvl) values ('Designer', 25, 100);
 
-insert into employee values ('PTC11962M', 'Philip', 'T', 'Cramer', 2, 215, '9952', '11/11/89');
-insert into employee values ('AMD15433F', 'Ann', 'M', 'Devon', 3, 200, '9952', '07/16/91');
-insert into employee values ('F-C16315M', 'Francisco', '', 'Chang', 4, 227, '9952', '11/03/90');
-insert into employee values ('LAL21447M', 'Laurence', 'A', 'Lebihan', 5, 175, '0736', '06/03/90');
-insert into employee values ('PXH22250M', 'Paul', 'X', 'Henriot', 5, 159, '0877', '08/19/93');
-insert into employee values ('SKO22412M', 'Sven', 'K', 'Ottlieb', 5, 150, '1389', '04/05/91');
-insert into employee values ('RBM23061F', 'Rita', 'B', 'Muller', 5, 198, '1622', '10/09/93');
-insert into employee values ('MJP25939M', 'Maria', 'J', 'Pontes', 5, 246, '1756', '03/01/89');
-insert into employee values ('JYL26161F', 'Janine', 'Y', 'Labrune', 5, 172, '9901', '05/26/91');
-insert into employee values ('CFH28514M', 'Carlos', 'F', 'Hernadez', 5, 211, '9999', '04/21/89');
-insert into employee values ('VPA30890F', 'Victoria', 'P', 'Ashworth', 6, 140, '0877', '09/13/90');
-insert into employee values ('L-B31947F', 'Lesley', '', 'Brown', 7, 120, '0877', '02/13/91');
-insert into employee values ('ARD36773F', 'Anabela', 'R', 'Domingues', 8, 100, '0877', '01/27/93');
-insert into employee values ('M-R38834F', 'Martine', '', 'Rance', 9, 75, '0877', '02/05/92');
-insert into employee values ('PHF38899M', 'Peter', 'H', 'Franken', 10, 75, '0877', '05/17/92');
-insert into employee values ('DBT39435M', 'Daniel', 'B', 'Tonini', 11, 75, '0877', '01/01/90');
-insert into employee values ('H-B39728F', 'Helen', '', 'Bennett', 12, 35, '0877', '09/21/89');
-insert into employee values ('PMA42628M', 'Paolo', 'M', 'Accorti', 13, 35, '0877', '08/27/92');
-insert into employee values ('ENL44273F', 'Elizabeth', 'N', 'Lincoln', 14, 35, '0877', '07/24/90');
+insert into employee values ('PTC11962M', 'Philip', 'T', 'Cramer', 2, 215, '9952', '89/11/11');
+insert into employee values ('AMD15433F', 'Ann', 'M', 'Devon', 3, 200, '9952', '91/07/16');
+insert into employee values ('F-C16315M', 'Francisco', '', 'Chang', 4, 227, '9952', '90/11/03');
+insert into employee values ('LAL21447M', 'Laurence', 'A', 'Lebihan', 5, 175, '0736', '90/06/03');
+insert into employee values ('PXH22250M', 'Paul', 'X', 'Henriot', 5, 159, '0877', '93/08/19');
+insert into employee values ('SKO22412M', 'Sven', 'K', 'Ottlieb', 5, 150, '1389', '91/04/05');
+insert into employee values ('RBM23061F', 'Rita', 'B', 'Muller', 5, 198, '1622', '93/10/09');
+insert into employee values ('MJP25939M', 'Maria', 'J', 'Pontes', 5, 246, '1756', '89/03/01');
+insert into employee values ('JYL26161F', 'Janine', 'Y', 'Labrune', 5, 172, '9901', '91/05/26');
+insert into employee values ('CFH28514M', 'Carlos', 'F', 'Hernadez', 5, 211, '9999', '89/04/21');
+insert into employee values ('VPA30890F', 'Victoria', 'P', 'Ashworth', 6, 140, '0877', '90/09/13');
+insert into employee values ('L-B31947F', 'Lesley', '', 'Brown', 7, 120, '0877', '91/02/13');
+insert into employee values ('ARD36773F', 'Anabela', 'R', 'Domingues', 8, 100, '0877', '93/01/27');
+insert into employee values ('M-R38834F', 'Martine', '', 'Rance', 9, 75, '0877', '92/02/05');
+insert into employee values ('PHF38899M', 'Peter', 'H', 'Franken', 10, 75, '0877', '92/05/17');
+insert into employee values ('DBT39435M', 'Daniel', 'B', 'Tonini', 11, 75, '0877', '90/01/01');
+insert into employee values ('H-B39728F', 'Helen', '', 'Bennett', 12, 35, '0877', '89/09/21');
+insert into employee values ('PMA42628M', 'Paolo', 'M', 'Accorti', 13, 35, '0877', '92/08/27');
+insert into employee values ('ENL44273F', 'Elizabeth', 'N', 'Lincoln', 14, 35, '0877', '90/07/24');
 
-insert into employee values ('MGK44605M', 'Matti', 'G', 'Karttunen', 6, 220, '0736', '05/01/94');
-insert into employee values ('PDI47470M', 'Palle', 'D', 'Ibsen', 7, 195, '0736', '05/09/93');
-insert into employee values ('MMS49649F', 'Mary', 'M', 'Saveley', 8, 175, '0736', '06/29/93');
-insert into employee values ('GHT50241M', 'Gary', 'H', 'Thomas', 9, 170, '0736', '08/09/88');
-insert into employee values ('MFS52347M', 'Martin', 'F', 'Sommer', 10, 165, '0736', '04/13/90');
-insert into employee values ('R-M53550M', 'Roland', '', 'Mendel', 11, 150, '0736', '09/05/91');
-insert into employee values ('HAS54740M', 'Howard', 'A', 'Snyder', 12, 100, '0736', '11/19/88');
-insert into employee values ('TPO55093M', 'Timothy', 'P', 'O''Rourke', 13, 100, '0736', '06/19/88');
-insert into employee values ('KFJ64308F', 'Karin', 'F', 'Josephs', 14, 100, '0736', '10/17/92');
-insert into employee values ('DWR65030M', 'Diego', 'W', 'Roel', 6, 192, '1389', '12/16/91');
-insert into employee values ('M-L67958F', 'Maria', '', 'Larsson', 7, 135, '1389', '03/27/92');
-insert into employee values ('PSP68661F', 'Paula', 'S', 'Parente', 8, 125, '1389', '01/19/94');
-insert into employee values ('MAS70474F', 'Margaret', 'A', 'Smith', 9, 78, '1389', '09/29/88');
-insert into employee values ('A-C71970F', 'Aria', '', 'Cruz', 10, 87, '1389', '10/26/91');
-insert into employee values ('MAP77183M', 'Miguel', 'A', 'Paolino', 11, 112, '1389', '12/07/92');
-insert into employee values ('Y-L77953M', 'Yoshi', '', 'Latimer', 12, 32, '1389', '06/11/89');
-insert into employee values ('CGS88322F', 'Carine', 'G', 'Schmitt', 13, 64, '1389', '07/07/92');
-insert into employee values ('PSA89086M', 'Pedro', 'S', 'Afonso', 14, 89, '1389', '12/24/90');
-insert into employee values ('A-R89858F', 'Annette', '', 'Roulet', 6, 152, '9999', '02/21/90');
-insert into employee values ('HAN90777M', 'Helvetius', 'A', 'Nagy', 7, 120, '9999', '03/19/93');
-insert into employee values ('M-P91209M', 'Manuel', '', 'Pereira', 8, 101, '9999', '01/09/89');
-insert into employee values ('KJJ92907F', 'Karla', 'J', 'Jablonski', 9, 170, '9999', '03/11/94');
-insert into employee values ('POK93028M', 'Pirkko', 'O', 'Koskitalo', 10, 80, '9999', '11/29/93');
-insert into employee values ('PCM98509F', 'Patricia', 'C', 'McKenna', 11, 150, '9999', '08/01/89');
+insert into employee values ('MGK44605M', 'Matti', 'G', 'Karttunen', 6, 220, '0736', '94/05/01');
+insert into employee values ('PDI47470M', 'Palle', 'D', 'Ibsen', 7, 195, '0736', '93/05/09');
+insert into employee values ('MMS49649F', 'Mary', 'M', 'Saveley', 8, 175, '0736', '93/06/29');
+insert into employee values ('GHT50241M', 'Gary', 'H', 'Thomas', 9, 170, '0736', '88/08/09');
+insert into employee values ('MFS52347M', 'Martin', 'F', 'Sommer', 10, 165, '0736', '90/04/13');
+insert into employee values ('R-M53550M', 'Roland', '', 'Mendel', 11, 150, '0736', '91/09/05');
+insert into employee values ('HAS54740M', 'Howard', 'A', 'Snyder', 12, 100, '0736', '88/11/19');
+insert into employee values ('TPO55093M', 'Timothy', 'P', 'O''Rourke', 13, 100, '0736', '88/06/19');
+insert into employee values ('KFJ64308F', 'Karin', 'F', 'Josephs', 14, 100, '0736', '92/10/17');
+insert into employee values ('DWR65030M', 'Diego', 'W', 'Roel', 6, 192, '1389', '91/12/16');
+insert into employee values ('M-L67958F', 'Maria', '', 'Larsson', 7, 135, '1389', '92/03/27');
+insert into employee values ('PSP68661F', 'Paula', 'S', 'Parente', 8, 125, '1389', '94/01/19');
+insert into employee values ('MAS70474F', 'Margaret', 'A', 'Smith', 9, 78, '1389', '88/09/29');
+insert into employee values ('A-C71970F', 'Aria', '', 'Cruz', 10, 87, '1389', '91/10/26');
+insert into employee values ('MAP77183M', 'Miguel', 'A', 'Paolino', 11, 112, '1389', '92/12/07');
+insert into employee values ('Y-L77953M', 'Yoshi', '', 'Latimer', 12, 32, '1389', '89/06/11');
+insert into employee values ('CGS88322F', 'Carine', 'G', 'Schmitt', 13, 64, '1389', '92/07/07');
+insert into employee values ('PSA89086M', 'Pedro', 'S', 'Afonso', 14, 89, '1389', '90/12/24');
+insert into employee values ('A-R89858F', 'Annette', '', 'Roulet', 6, 152, '9999', '90/02/21');
+insert into employee values ('HAN90777M', 'Helvetius', 'A', 'Nagy', 7, 120, '9999', '93/03/19');
+insert into employee values ('M-P91209M', 'Manuel', '', 'Pereira', 8, 101, '9999', '89/01/09');
+insert into employee values ('KJJ92907F', 'Karla', 'J', 'Jablonski', 9, 170, '9999', '94/03/11');
+insert into employee values ('POK93028M', 'Pirkko', 'O', 'Koskitalo', 10, 80, '9999', '93/11/29');
+insert into employee values ('PCM98509F', 'Patricia', 'C', 'McKenna', 11, 150, '9999', '89/08/01');
 
 CREATE  INDEX employee_ind ON employee(lname, fname, minit);
 CREATE  INDEX aunmind ON authors (au_lname, au_fname);
